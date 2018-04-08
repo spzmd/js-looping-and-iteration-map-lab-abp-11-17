1661e06918a7ecd59bac3cbbe6dea0033853df50
spzmd <b.keithdavid@gmail.com>
2018-04-08 13:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file.
-function lowerCaseDrivers(array) {
-  return array.map(function(e){
-    return e.toLowerCase()
+function lowerCaseDrivers(list) {
+  return list.map(function(driver){
+    return driver.toLowerCase()
   })
 }
 
