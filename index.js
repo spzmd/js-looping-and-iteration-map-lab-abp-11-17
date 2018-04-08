cf07f24f62cd6ad0e76f8d6f20cb4cbed9fdc21f
spzmd <b.keithdavid@gmail.com>
2018-04-08 13:40:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ function lowerCaseDrivers(array) {
 
 function nameToAttributes(names){
   return names.map(function(name) {
-
-    debugger
-    
     let fName = name.split(" ")[0]
     let lName = name.split(" ")[1]
     return {firstName: fName, lastName: lName}
