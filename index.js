51f02d775b98f44fa51aed22d8b8a2af7ebbf3de
spzmd <b.keithdavid@gmail.com>
2018-04-08 13:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file.
-const drivers = [
+
+
+/*const drivers = [
     {
       name: 'Bobby',
       hometown: 'Pittsburgh' },
@@ -21,7 +23,7 @@ function lowerCaseDrivers(drivers, string) {
   return drivers.push(driver => driver.toLowerCase() === string.toLowerCase() );
 }
 
-/*function fuzzyMatch(drivers, string) {
+function fuzzyMatch(drivers, string) {
   return drivers.filter(driver => driver.slice(0, string.length) === string);
 }
 
