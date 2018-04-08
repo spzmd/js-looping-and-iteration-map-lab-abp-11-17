2173d0ed1fb6e988a29a4328f07c95836ef36926
spzmd <b.keithdavid@gmail.com>
2018-04-08 13:21:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file.
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function lowerCaseDrivers(drivers, string) {
+  return drivers.push(driver => driver.toLowerCase() === string.toLowerCase() );
+}
+
+/*function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.slice(0, string.length) === string);
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string);
+}*/
